ec6efd282ad14a83510be7cc1b774383a07e570f
glmapper <glmapper_2018@163.com>
2025-05-29 10:43:58 +0800

    add jdbc ChatMemory

---
+++ spring-ai-chat-memory/spring-ai-chat-memory-jdbc/src/main/java/com/glmapper/ai/chat/memory/jdbc/JdbcMemoryApplication.java
@@ -0,0 +1,14 @@
+package com.glmapper.ai.chat.memory.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class JdbcMemoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcMemoryApplication.class, args);
+    }
+}
