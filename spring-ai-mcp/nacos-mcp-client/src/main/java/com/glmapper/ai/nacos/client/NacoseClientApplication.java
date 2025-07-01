d06c48ee284ac4355627d5f1069708064114c6d9
liu-657667 <liu-657667@qq.com>
2025-07-01 08:21:44 +0800

    refactor(nacos): 简化 Nacos 应用代码结构
    - 移除了 Nacos客户端和服务器应用中的未使用导入
    - 删除了 Nacos 客户端应用中的冗余代码片段
    - 清理了 Nacos 服务器应用中的多余导入和代码

--- spring-ai-mcp/nacos-mcp-client/src/main/java/com/glmapper/ai/nacos/client/NacoseClientApplication.java
+++ spring-ai-mcp/nacos-mcp-client/src/main/java/com/glmapper/ai/nacos/client/NacoseClientApplication.java
@@ -1,15 +1,7 @@
 package com.glmapper.ai.nacos.client;
 
-import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class NacoseClientApplication {
--- spring-ai-mcp/nacos-mcp-server/src/main/java/com/glmapper/ai/nacos/server/NacoseServerApplication.java
+++ spring-ai-mcp/nacos-mcp-server/src/main/java/com/glmapper/ai/nacos/server/NacoseServerApplication.java
@@ -1,18 +1,11 @@
 package com.glmapper.ai.nacos.server;
 
 import com.glmapper.ai.nacos.server.service.WeatherService;
-import io.modelcontextprotocol.server.McpServerFeatures;
-import io.modelcontextprotocol.spec.McpSchema;
-import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.function.FunctionToolCallback;
 import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class NacoseServerApplication {
